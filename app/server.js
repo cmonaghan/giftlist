b84a740dee4b80cd43b6316c36e35444780b6767
Christian Monaghan <christian.monaghan@gmail.com>
2014-03-20 21:54:24 -0700

    Added server to test amazon aws calls

---
+++ app/server.js
@@ -0,0 +1,14 @@
+var aws = require("aws-lib");
+
+var prodAdv = aws.createProdAdvClient('AKIAJA3VUOAVLK3I6EGA', 'cUIObeSHDv5BXuolmIyyCRVpFwoGYJzqImvIHv', 'giftlist08-20');
+
+var options = {SearchIndex: "Books", Keywords: "Javascript"}
+
+prodAdv.call("ItemSearch", options, function(err, result) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
+
