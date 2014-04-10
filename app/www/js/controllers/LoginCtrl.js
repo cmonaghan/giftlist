c091df32e8beb58badeb374aff90391f828e0750
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-10 14:12:04 -0700

    Successfully logs in user to Parse via Facebook

--- app/www/js/controllers/LoginCtrl.js
+++ app/www/js/controllers/LoginCtrl.js
@@ -1,5 +1,18 @@
 angular.module('giftlist.controllers')
 
 .controller('LoginCtrl', function($scope, $firebase, FirebaseService) {
-  $scope.login = FirebaseService.facebookLogin;
+  // $scope.login = FirebaseService.facebookLogin;
+
+  Parse.FacebookUtils.logIn(null, {
+    success: function(user) {
+      if (!user.existed()) {
+        alert("User signed up and logged in through Facebook!");
+      } else {
+        alert("User logged in through Facebook!");
+      }
+    },
+    error: function(user, error) {
+      alert("User cancelled the Facebook login or did not fully authorize.");
+    }
+  });
 });
