0382d38fdad91f397174d1a28579291998c2233e
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-13 14:26:11 -0700

    Saves userId to Parse with each saved gift

--- app/www/js/services/WishListService.js
+++ app/www/js/services/WishListService.js
@@ -3,6 +3,32 @@ angular.module('giftlist.services')
 .factory('WishListService', function($q) {
 
   var wishList = {};
+  console.log('wishList was called');
+
+  // create a reference to the current user
+  var user = Parse.User.current();
+
+  // Simple syntax to create a new subclass of Parse.Object.
+  var UserGiftList = Parse.Object.extend('UserGiftList');
+  // Create a new instance of that class.
+  var userGiftList = new UserGiftList();
+
+
+  userGiftList.set('userId', user.id);
+  userGiftList.addUnique('savedGifts', '123xyz');
+
+  userGiftList.save(null, {
+    success: function(userGiftList) {
+      // Execute any logic that should take place after the object is saved.
+      alert('New object created with objectId: ' + userGiftList.id);
+    },
+    error: function(userGiftList, error) {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and description.
+      alert('Failed to create new object, with error code: ' + error.description);
+    }
+  });
+
 
   return {
     addToWishList: function(currentGift) {
