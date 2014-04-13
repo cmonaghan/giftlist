db94becf647b2402d7a348ca1a1bb945d70219ad
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-12 17:26:02 -0700

    Deleted WishListService code related to Firebase

--- app/www/js/services/WishListService.js
+++ app/www/js/services/WishListService.js
@@ -1,31 +1,7 @@
 angular.module('giftlist.services')
 
 .factory('WishListService', function($http, $q, $firebase, FirebaseService) {
-  // var url = 'http://giftlist-api.herokuapp.com/macys/v3~catalog~category~118~browseproducts?resultsperpage=10';
 
-  // return {
-  //   wishList: [],
-
-  //   getWishList: function() {
-  //     var deferred = $q.defer();
-
-  //     $http.get(url)
-  //       .success(function(data) {
-  //         // data may need some processing?
-  //         this.wishList = data;
-  //         console.log(this.wishlist);
-  //         deferred.resolve();
-  //       })
-  //       .error(function(data, status) {
-  //         deferred.reject(data, status);
-  //       });
-
-  //     return deferred.promise;
-  //   }
-  // };
-  //
-  // debugger;
-  // var wishListRef = FirebaseService.userRef.child('wishlist');
   var wishList = {};
 
   var deferred = $q.defer();
@@ -54,21 +30,4 @@ angular.module('giftlist.services')
   });
 
   return deferred.promise;
-
-  // var wishListRef = FirebaseService.wishListRef;
-  // var wishlistRef = $firebase(FirebaseService.wishlistRef);
-  // var wishList = [];
 });
-
-// var wishlistRef = $firebase(FirebaseService.wishlistRef);
-// // var wishList = [];
-
-// return {
-//   addToWishList: function(currentGift) {
-//     debugger;
-//     wishListRef.$add(currentGift);
-//   },
-//   getWishList: function() {
-//     return wishListRef;
-//   }
-// };
