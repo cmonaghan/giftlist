a50c422e9d4fb8f58e08d9514b6abcc269cdb1f4
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-13 15:57:16 -0700

    Creates a new UserGiftList if none exists for that user

--- app/www/js/services/WishListService.js
+++ app/www/js/services/WishListService.js
@@ -1,25 +1,41 @@
 angular.module('giftlist.services')
 
 .factory('WishListService', function($q) {
-
   var wishList = {};
 
   var saveItemToParseGiftList = function(gift) {
     // create a reference to the current user
     var user = Parse.User.current();
-
     // Create a reference to the Parse.Object 'UserGiftList'
     var UserGiftList = Parse.Object.extend('UserGiftList');
     var userGiftListQuery = new Parse.Query(UserGiftList);
     userGiftListQuery.equalTo('parent',user);
     userGiftListQuery.first({
       success: function(userGiftList){
-        console.log(userGiftList);
-        userGiftList.addUnique('savedGifts', gift.id);
-        userGiftList.save();
+        if (userGiftList === undefined) {
+          createNewUserGiftList(gift);
+        } else {
+          userGiftList.addUnique('savedGifts', gift.id);
+          userGiftList.save();
+        }
       },
       error: function() {
         console.error('Error in userGiftListQuery.');
+      }
+    });
+  };
+
+  var createNewUserGiftList = function(gift){
+    var user = Parse.User.current();
+    var UserGiftList = Parse.Object.extend('UserGiftList');
+    var userGiftList = new UserGiftList();
+    userGiftList.set('parent',user);
+    userGiftList.save({
+      success: function() {
+        saveItemToParseGiftList(gift);
+      },
+      error: function(error) {
+        console.error(error);
       }
     });
   };
