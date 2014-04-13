d80324373df64cc89cf6b605a7907ba91e24a4b1
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-13 14:51:49 -0700

    Saves to savedGifts array in Parse

--- app/www/js/services/WishListService.js
+++ app/www/js/services/WishListService.js
@@ -10,23 +10,38 @@ angular.module('giftlist.services')
   // Simple syntax to create a new subclass of Parse.Object.
   var UserGiftList = Parse.Object.extend('UserGiftList');
   // Create a new instance of that class.
-  var userGiftList = new UserGiftList();
+  // var userGiftList = new UserGiftList();
 
 
-  userGiftList.set('parent', user);
-  userGiftList.addUnique('savedGifts', '123xyz');
-
-  userGiftList.save(null, {
-    success: function(userGiftList) {
-      // Execute any logic that should take place after the object is saved.
-      alert('New object created with objectId: ' + userGiftList.id);
+  var query = new Parse.Query(UserGiftList);
+  query.get("fDzrCfMjYu", {
+    success: function(object) {
+      // The object was retrieved successfully.
+      object.addUnique('savedGifts', 'twladsg');
+      object.save();
     },
-    error: function(userGiftList, error) {
-      // Execute any logic that should take place if the save fails.
+    error: function(object, error) {
+      // The object was not retrieved successfully.
       // error is a Parse.Error with an error code and description.
-      alert('Failed to create new object, with error code: ' + error.description);
+      alert('Error fetching object!');
     }
   });
+
+
+  // userGiftList.save(null, {
+  //   success: function(userGiftList) {
+  //     userGiftList.set('parent', user);
+  //     userGiftList.addUnique('savedGifts', '456abc');
+  //     userGiftList.save();
+  //     // Execute any logic that should take place after the object is saved.
+  //     alert('New object created with objectId: ' + userGiftList.id);
+  //   },
+  //   error: function(userGiftList, error) {
+  //     // Execute any logic that should take place if the save fails.
+  //     // error is a Parse.Error with an error code and description.
+  //     alert('Failed to create new object, with error code: ' + error.description);
+  //   }
+  // });
 
 
   return {
