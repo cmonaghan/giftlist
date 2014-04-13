f27b2e665b49b17cd0edb8dd5fa81b79702362d2
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-13 15:38:33 -0700

    Saves giftId to Parse if a userGiftList already exists for that user

--- app/www/js/services/WishListService.js
+++ app/www/js/services/WishListService.js
@@ -4,77 +4,30 @@ angular.module('giftlist.services')
 
   var wishList = {};
 
-  // create a reference to the current user
-  var user = Parse.User.current();
+  var saveItemToParseGiftList = function(gift) {
+    // create a reference to the current user
+    var user = Parse.User.current();
 
-  // Simple syntax to create a new subclass of Parse.Object.
-  var UserGiftList = Parse.Object.extend('UserGiftList');
-  // Create a new instance of that class.
-  // var userGiftList = new UserGiftList();
-
-  var userGiftListQuery = new Parse.Query(UserGiftList);
-  userGiftListQuery.equalTo('parent',user);
-  userGiftListQuery.find({
-    success: function(userGiftList){
-      console.log('userGiftList',userGiftList);
-      var userGiftListId = userGiftList.id;
-      alert('success!');
-      return userGiftList;
-    },
-    error: function() {
-      alert('error!');
-    }
-  }).then(function(result){
-    var query = new Parse.Query(UserGiftList);
-    query.get(result[0].id, {
-      success: function(object) {
-        // The object was retrieved successfully.
-        object.addUnique('savedGifts', 'face');
-        object.save();
+    // Create a reference to the Parse.Object 'UserGiftList'
+    var UserGiftList = Parse.Object.extend('UserGiftList');
+    var userGiftListQuery = new Parse.Query(UserGiftList);
+    userGiftListQuery.equalTo('parent',user);
+    userGiftListQuery.first({
+      success: function(userGiftList){
+        console.log(userGiftList);
+        userGiftList.addUnique('savedGifts', gift.id);
+        userGiftList.save();
       },
-      error: function(object, error) {
-        // The object was not retrieved successfully.
-        // error is a Parse.Error with an error code and description.
-        alert('Error fetching object!');
+      error: function() {
+        console.error('Error in userGiftListQuery.');
       }
     });
-  });
-
-
-  // var query = new Parse.Query(UserGiftList);
-  // query.get("fDzrCfMjYu", {
-  //   success: function(object) {
-  //     // The object was retrieved successfully.
-  //     object.addUnique('savedGifts', 'twladsg');
-  //     object.save();
-  //   },
-  //   error: function(object, error) {
-  //     // The object was not retrieved successfully.
-  //     // error is a Parse.Error with an error code and description.
-  //     alert('Error fetching object!');
-  //   }
-  // });
-
-
-  // userGiftList.save(null, {
-  //   success: function(userGiftList) {
-  //     userGiftList.set('parent', user);
-  //     userGiftList.addUnique('savedGifts', '456abc');
-  //     userGiftList.save();
-  //     // Execute any logic that should take place after the object is saved.
-  //     alert('New object created with objectId: ' + userGiftList.id);
-  //   },
-  //   error: function(userGiftList, error) {
-  //     // Execute any logic that should take place if the save fails.
-  //     // error is a Parse.Error with an error code and description.
-  //     alert('Failed to create new object, with error code: ' + error.description);
-  //   }
-  // });
-
+  };
 
   return {
     addToWishList: function(currentGift) {
       wishList[currentGift.id] = currentGift;
+      saveItemToParseGiftList(currentGift);
     },
     getWishList: function() {
       return wishList;
