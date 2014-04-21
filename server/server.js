39cfda634d824094228bc698d552a981c2f72779
Christian Monaghan <christian.monaghan@gmail.com>
2014-04-20 22:04:51 -0700

    Server can now loop through multiple Amazon pages and save to Parse

--- server/server.js
+++ server/server.js
@@ -3,23 +3,29 @@ var helpers = require('./helpers.js');
 
 // AWS stuff
 var amazonAPI = aws.createProdAdvClient('AKIAJA3VUOAVLK3I6EGA', '6+cUIObeSHDv5BXuolmIyyCRVpFwoGYJzqImvIHv', 'giftlist08-20');
-var options = {
-  SearchIndex: "HomeGarden",
-  Keywords: "wedding registry",
-  ResponseGroup: 'Medium'
+
+saveAmazonItemsToParse('HomeGarden', 'wedding registry', 'Medium', 10);
+
+function saveAmazonItemsToParse (SearchIndex, Keywords, ResponseGroup, numberOfPages) {
+  for (var pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
+    // each loop will increment the 'ItemPage' value in order to get multiple pages
+    // Amazon allows ItemPage to be a max of 10
+    var options = {
+      SearchIndex: SearchIndex,
+      Keywords: Keywords,
+      ResponseGroup: ResponseGroup,
+      ItemPage: pageNumber
+    };
+
+    amazonAPI.call("ItemSearch", options, function(err, response) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Amazon request isValid: ' + response.Items.Request.IsValid);
+        console.log('Total results fetched: ' + response.Items.Item.length);
+        console.log('Total results found: ' + response.Items.TotalResults);
+        helpers.saveDataToParse(response);
+      }
+    });
+  };
 };
-
-
-amazonAPI.call("ItemSearch", options, function(err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    // running `node-debug server.js` (node-inspector) allows you to view response in browser
-
-    // will print 'Amazon Request IsValid: {boolean}'
-    console.log('Amazon request isValid: ' + response.Items.Request.IsValid);
-    console.log('Total results fetched: ' + response.Items.Item.length);
-    console.log('Total results found: ' + response.Items.TotalResults);
-    helpers.saveDataToParse(response);
-  }
-});
